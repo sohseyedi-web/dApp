a25366f8a977a12003626fbd24a32a0762fcd88c
sohseyedi <sohseyedi@gmail.com>
2023-10-31 11:04:40 +0330

    add tokenlist item

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="gap-x-4 max-w-7xl w-full mx-auto flex items-center justify-center py-3 border-t-2 border-gray-400">
-      <Link to={"/"}>
+      <Link to={"https://github.com/sohseyedi-web/dApp"}>
         <RiIcon.RiGithubFill
           size={30}
           className="text-indigo-700 transition-all duration-300 hover:text-blue-700"
